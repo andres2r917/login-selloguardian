3346717a44c589c288f3c8a88b8c808314f16e10
andres2r917 <dav9095551@gmail.com>
2026-04-23 17:29:30 -0500

    Version3

--- Login/src/Componentes/Navbar.jsx
+++ Login/src/Componentes/Navbar.jsx
@@ -6,6 +6,9 @@ export const Navbar = () => {
       <div>
         <img className='fondo' src="https://tse4.mm.bing.net/th/id/OIP.X6xQSueBbNbJ3Nq17LZKdQHaE5?rs=1&pid=ImgDetMain&o=7&rm=3" alt="" />
       </div>
+         <div>
+        <img src="./assets/" alt="" />
+         </div>
     </div>
   );
 }
