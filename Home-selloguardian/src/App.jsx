ea844464c0fc3012f953d229f5d508a0fb7d840d
andres2r917 <dav9095551@gmail.com>
2026-04-25 17:31:35 -0500

    login

---
+++ Home-selloguardian/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header  from './Componentes/Header.jsx'
+import Navbar from './Componentes/navbar.jsx'
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+    
+
+    </div>
+  )
+}
+
+export default App
---
+++ Home-selloguardian/src/Componentes/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './Navbar.css'
+const navbar = () => {
+  return (
+    <div>
+         <navbar className="main-navbar">
+      <nav className="nav-bar">
+      
+        <div className="nav-links">
+          <a href="#inicio" className="nav-item active">Inicio</a>
+          <a href="#adopta" className="nav-item">Adopta</a>
+          <a href="#denuncia" className="nav-item">Denuncia</a>
+        </div>
+
+       
+        <div className="search-container">
+          <div className="search-input-wrapper">
+            <input type="text" placeholder="¿Qué buscas?" />
+            <button className="search-button">🔍</button>
+          </div>
+        </div>
+
+      
+        <div className="auth-buttons">
+          <a href="#login" className="login-btn">Inicia sesión</a>
+          <button className="register-btn">Registrarse</button>
+        </div>
+      </nav>
+    </navbar>
+    </div>
+  )
+}
+
+export default navbar
